34987b22b94e22ad3f55c67a319be34938edd741
Jonas Schwegler <jonas.c.schwegler@gmail.com>
2023-11-20 15:44:07 +0000

    first attempts of creating the app routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  get '/goats', to: 'goats#index'
+
+  get '/goats/:id/bookings/new', to: 'bookings#new'
+
+  post '/bookings', to: 'bookings#create'
+
+  get '/dashboard', to: 'users#dashboard'
+
+  resources :goats, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "posts#index"
