757da9b8cba1848d296ed49be6b12f3aae66127b
Jonas Schwegler <jonas.c.schwegler@gmail.com>
2023-11-22 12:22:53 +0000

    Did the login/logout with alerts

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :set_booking, only: [:destroy]
 
   def new
     @booking = Booking.new
@@ -13,6 +13,9 @@ class BookingsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def dashboard
   end
 
   def destroy
