1b58bc06c1db08dac4d78faf7c99ca19b210dc2f
Gagan Sandhu <gagan27sandhu@gmail.com>
2023-11-24 14:00:25 +0000

    fixed the popup info

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,7 +22,7 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)// Add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // Add this
